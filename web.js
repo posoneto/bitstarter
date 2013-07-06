e43406f3d3dc1012374358546e7e5b2cfdccd66e
posoneto <walter_figueroa@hotmail.com>
2013-07-06 18:00:28 -0500

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+var inputFile = fs.readFileSync("index.html", 'utf8');
+//var buffer = new Buffer(inputFile);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    
+    response.send(inputFile);
 });
 
 var port = process.env.PORT || 5000;
